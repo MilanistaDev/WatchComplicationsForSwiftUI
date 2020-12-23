736c2eb153cb27cd5a933bcbeed161ad79856249
Takuya Aso <milanista224@gmail.com>
2020-12-23 22:30:55 +0900

    Add Dummy operation data.

--- WatchComplicationsForSwiftUI WatchKit Extension/Model/OperationSchedule.swift
+++ WatchComplicationsForSwiftUI WatchKit Extension/Model/OperationSchedule.swift
@@ -13,3 +13,12 @@ struct OperationSchedule {
     var finalStaIcon: String
     var finalStaName: String
 }
+
+let sampleData: [OperationSchedule] = [
+    OperationSchedule(arrivalTime: "8:20", trainType: .local,
+                      finalStaIcon: "T07", finalStaName: "九段下"),
+    OperationSchedule(arrivalTime: "8:24", trainType: .rapidCommuting,
+                      finalStaIcon: "T01", finalStaName: "中野"),
+    OperationSchedule(arrivalTime: "8:28", trainType: .rapid,
+                      finalStaIcon: "JB01", finalStaName: "三鷹")
+]
