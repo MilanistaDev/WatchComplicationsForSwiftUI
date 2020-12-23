c5bfa5cea60b30ab34090db64a3c0577d3618b02
Takuya Aso <milanista224@gmail.com>
2020-12-23 22:28:17 +0900

    Add Model of Operation Schedule.

---
+++ WatchComplicationsForSwiftUI WatchKit Extension/Model/OperationSchedule.swift
@@ -0,0 +1,15 @@
+//
+//  OperationSchedule.swift
+//  WatchComplicationsForSwiftUI WatchKit Extension
+//
+//  Created by Takuya Aso on 2020/12/23.
+//
+
+import Foundation
+
+struct OperationSchedule {
+    var arrivalTime: String
+    var trainType: TrainType
+    var finalStaIcon: String
+    var finalStaName: String
+}
