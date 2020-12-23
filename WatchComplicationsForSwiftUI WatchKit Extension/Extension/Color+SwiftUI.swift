8df09822f1844853a70183104e3295009cd258e7
Takuya Aso <milanista224@gmail.com>
2020-12-23 22:26:02 +0900

    Add Color_s Extension.

---
+++ WatchComplicationsForSwiftUI WatchKit Extension/Extension/Color+SwiftUI.swift
@@ -0,0 +1,12 @@
+//
+//  Color+SwiftUI.swift
+//  WatchComplicationsForSwiftUI WatchKit Extension
+//
+//  Created by Takuya Aso on 2020/12/23.
+//
+
+import SwiftUI
+
+extension Color {
+    static let noticeBg = Color(UIColor(named: "noticeBg")!)
+}
